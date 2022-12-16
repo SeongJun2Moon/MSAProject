531eee7f7e4e3e46e732f66a726389f0e3fd47bc
SJ <sjmun0420@gmail.com>
2022-12-16 19:00:01 +0900

    22.12.16. 19:00

---
+++ DjangoServer/shop/fashion/migrations/urls.py
@@ -0,0 +1,7 @@
+from django.urls import re_path as url
+from shop.fashion2 import views
+
+urlpatterns = [
+    url(r'aa', views.fashion)
+]
+
---
+++ ReactServer/src/shop/index.js
@@ -0,0 +1,2 @@
+export {default as Iris} from './components/Iris'
+export {default as Fashion} from './components/Fashion'
