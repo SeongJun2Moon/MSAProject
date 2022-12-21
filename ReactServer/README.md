fc39759ebe5185f1afa03911124dc96534b9a3e7
SJMoon <103993019+CentiHei@users.noreply.github.com>
2022-12-22 06:34:25 +0900

    리액트 리드미 작성

--- ReactServer/README.md
+++ ReactServer/README.md
@@ -1,7 +1,13 @@
-## 에러 확인 팁
+# 목록
+
+## 코드 내 이론
+
+- <a href="https://github.com/SeongJun2Moon/MSAProject/tree/main/ReactServer/src/webcrawler">Promise로 데이터호출</a>
+
+## 이론
+
+### 에러 구분 팁
+
 - 2-- : 정상작동
 - 4-- : 클라이언트 에러
 - 5-- : 서버 에러
-
-## 판다스로 리액에서 mysql로 df보내기
-https://swalloow.github.io/dataframe-to-mysql/
--- ReactServer/src/webcrawler/components/NaverMovies.jsx
+++ ReactServer/src/webcrawler/components/NaverMovies.jsx
@@ -1,25 +1,3 @@
-// import { getNaverMovie } from "webcrawler/api";
-
-// const NaverMovie = () => {
-
-//   const onClick = (e) => {
-//     e.preventDefault();
-//     getNaverMovie().then((response) => {
-//     console.log(`response is ${response.data.result}`);
-//       localStorage.setItem("token", JSON.stringify(response.data.result));
-//     });
-//   };
-
-//   return (
-//     <>
-//       <h2>네이버 크롤러</h2>
-//       <button onClick={onClick}>네이버 영화 크롤링</button>
-//       <p>버튼을 클릭하시면, 네이버 영화 목록이 출력됩니다.</p>
-//     </>
-//   );
-// };
-// export default NaverMovie;
-
 import { useState } from "react";
 import webcrawlerService from "../api";
 const NaverMovie = () => {
